9706de0cafa945e03c316395b0a0eef7d1466472
tv-dem <69cobak@mail.ru>
2020-02-20 21:52:36 +0300

    feat: task comlit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 
 exports.min = function min (array) {
+  if (Array.isArray(array)){
+    return array.length === 0 ? 0 : Math.min(...array);
+  }
   return 0;
 }
 
 exports.max = function max (array) {
+  if (Array.isArray(array)){
+    return array.length === 0 ? 0 : Math.max(...array);
+  }
   return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(Array.isArray(array)){
+  return array.length === 0 ? 0 : array.reduce((akk, el) => {
+    return akk + el;
+  }, 0)/array.length
+}
+  else return 0;
 }
